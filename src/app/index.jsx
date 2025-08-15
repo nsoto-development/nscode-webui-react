dec82b72389857a9fa4a8d63aaf6691537b4aa20
Nelson Soto <nsoto.development@gmail.com>
2025-08-15 15:46:10 -0400

    renamed and moved to src\app

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,13 +1,17 @@
-import { StrictMode } from "react";
+// src/app/index.jsx
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+// Global CSS – you can keep it in src/styles/
 import "../styles/index.css";
-import App from "../App.jsx";
-import { ChatProvider } from "../context/ChatContext.jsx";
+
+import ChatPage from "../pages/ChatPage.jsx";
+import { ChatProvider } from "./providers/ChatProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ChatProvider>
-      <App />
+      <ChatPage />
     </ChatProvider>
   </StrictMode>
 );
