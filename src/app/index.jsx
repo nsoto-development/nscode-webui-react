f0161c5c6a70c509b2a55e7304af9a2d7cfd45c5
Nelson Soto <nsoto.development@gmail.com>
2025-08-15 14:47:48 -0400

    chore: rename entry point to src/app/index.jsx and update index.html

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { ChatProvider } from "./context/ChatContext.jsx";
+import "../styles/index.css";
+import App from "../App.jsx";
+import { ChatProvider } from "../context/ChatContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
